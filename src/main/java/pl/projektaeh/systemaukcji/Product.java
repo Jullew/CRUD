3a63466297275f88974640a66e91f6230f16305a
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2023-03-15 15:32:22 +0100

    `Some important changes`

--- src/main/java/pl/projektaeh/systemaukcji/Product.java
+++ src/main/java/pl/projektaeh/systemaukcji/Product.java
@@ -1,20 +1,21 @@
-//package pl.projektaeh.systemaukcji;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.util.Date;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Product {
-//    private int id;
-//    private String idWlasciciela;
-//    private String tytul;
-//    private String opis;
-//    private double cena;
-//    private String idUzytkownika;
-//    private Date dataKoniec;
-//}
+package pl.projektaeh.systemaukcji;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private Long idCustomer;
+    private String title;
+    private String desc;
+    private double price;
+    private Long userId;
+    private LocalDate dateEnd;
+}
--- src/main/java/pl/projektaeh/systemaukcji/User.java
+++ src/main/java/pl/projektaeh/systemaukcji/User.java
@@ -1,22 +1,31 @@
 package pl.projektaeh.systemaukcji;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
-    private int id_user;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long idUser;
     private String login;
     private String password;
     private String name;
     private String surname;
-    private String key_api;
-    private String key_session;
-    private Date session_end;
+    private String keyApi;
+    private String keySession;
+    private LocalDate sessionEnd;
 }
 
