4678c46f260f68b07d452c8877d6dbf895f3145e
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2023-05-10 20:29:33 +0200

    Usunięcie pola apiKey w klasie User

--- src/main/java/pl/aeh_project/auction_system/domain/entity/User.java
+++ src/main/java/pl/aeh_project/auction_system/domain/entity/User.java
@@ -27,7 +27,6 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
-    private String apiKey;
     private String sessionKey;
     private LocalDateTime sessionEnd;
 
